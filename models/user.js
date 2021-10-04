46db27f400b0fb1d408db15eef4732150638116e
BenVonAchen <bennyvonachen@gmail.com>
2021-10-04 09:25:27 -0600

    Fixed minor syntax errors which caused the SignUp post method to not operate correctly

--- models/user.js
+++ models/user.js
@@ -1,48 +1,49 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       // this.belongsToMany(models.Inventory, { through: 'inventory_users' });
       this.hasMany(models.Inventory);
     }
-  };
-  User.init({
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false
+  }
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+      },
+      phoneNumber: {
+        type: DataTypes.STRING,
+      },
+      accountType: {
+        type: DataTypes.ENUM("seller", "buyer", "admin"),
+        allowNull: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    address: {
-      type: DataTypes.STRING
-    },
-    phoneNumber: {
-      type: DataTypes.STRING
-    },
-    accountType: {
-      type: DataTypes.ENUM('seller', 'buyer', 'admin'),
-      allowNull: true
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
+    {
+      sequelize,
+      modelName: "User",
     }
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  );
   return User;
 };
--- routes/users.js
+++ routes/users.js
@@ -1,40 +1,44 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var bcrypt = require('bcrypt');
-const { User } = require('../models');
-var auth = require('../services/auth');
+var bcrypt = require("bcrypt");
+const { User } = require("../models");
+var auth = require("../services/auth");
 
 /* GET all users */
-router.get('/', function(req, res, next) {
-  User.findAll().then(userList => {
+router.get("/", function (req, res, next) {
+  User.findAll().then((userList) => {
     res.json(userList);
-  })
+  });
 });
 
 /* GET /:id get an individual user */
-router.get('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   const userId = parseInt(req.params.id);
 
   User.findOne({
     where: {
-      id: userId
+      id: userId,
+    },
+  }).then(
+    (theUser) => {
+      if (theUser) {
+        res.json(theUser);
+      } else {
+        res.status(404).send();
+      }
+    },
+    (err) => {
+      res.status(500).send(err);
     }
-  }).then(theUser => {
-    if (theUser) {
-      res.json(theUser);
-    } else {
-      res.status(404).send();
-    }
-  }, err => {
-    res.status(500).send(err);
-  })
+  );
 });
 
 /* POST create a user */
-router.post('/', async (req, res, next) => {
-  
-  if (!req.body.username || !req.body.password) {
-    res.status(400).send('Username and Password Required');
+router.post("/", async (req, res, next) => {
+  console.log(req.body);
+
+  if (!req.body.userName || !req.body.password) {
+    res.status(400).send("Username and Password Required");
     return;
   }
 
@@ -42,99 +46,104 @@ router.post('/', async (req, res, next) => {
 
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(req.body.password, salt);
-  
+
   User.create({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
-    username: req.body.username,
+    username: req.body.userName,
     email: req.body.email,
     address: req.body.address,
     phoneNumber: req.body.phoneNumber,
     // accountType: req.body.accountType,
-    password: hashedPassword
+    password: hashedPassword,
   })
     .then((newUser) => {
       res.json(newUser);
-  })
+    })
     .catch(() => {
       res.status(400).send();
-  });
+    });
 });
 
 // POST SignIn a user */
-router.post('/signin', async ( req, res, next ) => {
+router.post("/signin", async (req, res, next) => {
   User.findOne({
     where: {
-      username: req.body.username
-    }
-  }).then(async user => {
+      username: req.body.username,
+    },
+  }).then(async (user) => {
     //check if user exists
     if (!user) {
-      res.status(404).send('Invalid username');
+      res.status(404).send("Invalid username");
       return;
     }
     //check the password
     const valid = await bcrypt.compare(req.body.password, user.password);
 
-    if (valid){
-    //create the token
+    if (valid) {
+      //create the token
       const jwt = auth.createJWT(user);
       res.status(200).send({ jwt });
     } else {
-      res.status(401).send('Invalid password');
+      res.status(401).send("Invalid password");
     }
-
   });
 });
 
 /* PUT update a user */
-router.put('/:id', ( req, res, next ) => {
+router.put("/:id", (req, res, next) => {
   const userId = parseInt(req.params.id);
 
   if (!userId || userId <= 0) {
-    res.status(400).send('Invalid ID');
+    res.status(400).send("Invalid ID");
     return;
   }
 
-  User.update({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    username: req.body.username,
-    email: req.body.email,
-    address: req.body.address,
-    phoneNumber: req.body.phoneNumber,
-    accountType: req.body.accountType,
-    password: req.body.password
-  }, {
-    where: {
-      id: userId
+  User.update(
+    {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      email: req.body.email,
+      address: req.body.address,
+      phoneNumber: req.body.phoneNumber,
+      accountType: req.body.accountType,
+      password: req.body.password,
+    },
+    {
+      where: {
+        id: userId,
+      },
     }
-  }).then(() => {
-    res.status(204).send();
-  }).catch(() => {
-    res.status(400).send();
-  })
+  )
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch(() => {
+      res.status(400).send();
+    });
 });
 
 /* DELETE a user */
-router.delete('/:id', ( req, res, next ) => {
+router.delete("/:id", (req, res, next) => {
   const userId = parseInt(req.params.id);
 
   if (!userId || userId <= 0) {
-    res.status(400).send('Invalid ID');
+    res.status(400).send("Invalid ID");
     return;
   }
 
   User.destroy({
     where: {
-      id: userId
-    }
-  }).then(() => {
-    res.status(204).send();
-  }).catch(() => {
-    res.status(400).send();
+      id: userId,
+    },
   })
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch(() => {
+      res.status(400).send();
+    });
 });
-
 
 module.exports = router;
